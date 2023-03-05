7bf60391bc86aed22efbd08684d17707f9a10fb4
ulugbekpy <ulugbekpycpp@gmail.com>
2023-03-05 05:00:45 +0500

    models created

---
+++ app/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+
+    def str(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField(null=True, blank=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True,
+                              upload_to='media/products/')
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=30)
+
+    def str(self):
+        return self.name
+
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.customer.name
+
+
+class OrderItem(models.Model):
+
+    class StatusChoice(models.TextChoices):
+        APPROVED = 'A'
+        INCONFIRMED = 'I'
+
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='orders')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=1, choices=StatusChoice.choices, default=StatusChoice.INCONFIRMED)
+
+    def str(self):
+        return self.product.title
+
+
+class Favourite(models.Model):
+    user = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def str(self):
+        return self.product.title
